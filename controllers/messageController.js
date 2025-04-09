db350ccd78b86a6acdeeb1bd301b50ceaa94fe14
adams <adamskiptalam0@gmail.com>
2025-04-09 17:50:35 +0300

    Add view and logic to render a single message to page

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -6,3 +6,24 @@ exports.index = (req, res) => {
 exports.newMessageForm = (req, res) => {
 	res.render("form", { title: "Add new message" });
 };
+
+exports.handleNewMessage = (req, res) => {
+	const { user, text } = req.body;
+
+	messages.push({
+		id: messages.length + 1, 
+		text: text,
+		user: user,
+		added: new Date(),
+	});
+	res.redirect("/");
+};
+
+exports.viewIndividualMessage = (req,res) => {
+	const { id } = req.params;
+	const message = messages.find((msg) => msg.id === id)
+	if (!message) {
+		return res.status(404).send("Message not found");
+	}
+	res.render('user', { message })
+};
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get("/", messageController.index);
 // get new message form
 router.get("/new", messageController.newMessageForm);
 
+// add a new message
+router.post("/new", messageController.handleNewMessage);
+
+// get individual message
+router.get("/:id", messageController.viewIndividualMessage);
+
 module.exports = router;
